c79f7f5a74a4e7dd25f485ecba26bba53e7cb0eb
danielliwag <danielarylliwag@gmail.com>
2025-04-12 05:59:25 +0800

    Work with pulling API data

---
+++ extract.py
@@ -0,0 +1,13 @@
+import requests
+from config import logger
+
+url = 'https://api.spacexdata.com/v5/launches'
+
+
+def extract_data(url):
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return data
+    except Exception as e:
+        logger.error(f'Problem occured during data ingestion: {e}')
