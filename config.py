2d1d498c3671ce0d450461d7f294b6c0c4a9cf19
danielliwag <danielarylliwag@gmail.com>
2025-04-14 18:37:22 +0800

    Updated the config for DB creds

--- config.py
+++ config.py
@@ -4,14 +4,20 @@ import logging
 
 load_dotenv()
 
+url = 'https://api.spacexdata.com/v5/launches'
+rocketurl =  'https://api.spacexdata.com/v4/rockets'
+launchpadurl = 'https://api.spacexdata.com/v4/launchpads'
 
-
-
-
-
+db_config = {
+    'user': os.getenv('DB_USER'),
+    'pass': os.getenv('DB_PASS'),
+    'host': os.getenv('DB_HOST'),
+    'port': os.getenv('DB_PORT'),
+    'name': os.getenv('DB_NAME'),
+}
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format= '%(asctime)s - %(levelname)s - %(message)s',
     handlers= [
         logging.FileHandler('pipeline.log'),
