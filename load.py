b69b1f83abe18f7cea46803f60809ebf5197e1d1
danielliwag <danielarylliwag@gmail.com>
2025-04-23 18:20:25 +0800

    Remove the dumping of json response and moved it to transform script

--- load.py
+++ load.py
@@ -2,18 +2,19 @@ from sqlalchemy import create_engine
 import json
 from config import logger
 
-def load_data(dataframe, db_config):
+def load_data(dataframe, DB_CONFIG):
     logger.info('Loading the dataframe to the database.')
     if not dataframe.empty:
         try:
-            for col in dataframe.columns:
-                if dataframe[col].apply(lambda x: isinstance(x, (dict, list))).any():
-                    dataframe[col] = dataframe[col].apply(json.dumps)
+            # for col in dataframe.columns:
+            #     if dataframe[col].apply(lambda x: isinstance(x, (dict, list))).any():
+            #         dataframe[col] = dataframe[col].apply(json.dumps)
 
-            engine = create_engine(f"postgresql://{db_config['user']}:{db_config['pass']}@{db_config['host']}:{db_config['port']}/{db_config['name']}")
+            engine = create_engine(f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['pass']}@{DB_CONFIG['host']}:{DB_CONFIG['port']}/{DB_CONFIG['name']}")
             dataframe.to_sql('spacex_launches', engine, if_exists ='replace', index= False)
+            logger.info('Data is successfully loaded into the database.')
         except Exception as e:
             logger.error(f'Problem occured during data loading. {e}')
     else:
         logger.warning('No data to load.')
-    logger.info('Data is successfully loaded into the database.')
+    
